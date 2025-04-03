fcd14c3589970a175435f9b3602742a600753733
Amr Hussain <amrohossnei@gmail.com>
2025-04-03 05:53:36 +0200

    enabled images in develpment mode

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path , include
+# to show the images in development mode 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 # from users.views import *
 from main.views import *
 urlpatterns = [
@@ -8,6 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('/', include('main.urls')),
     path ('chat', include('chatgpt.urls')),
-
-   
 ]
+if settings.DEBUG:  # Serve media files in development mode
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
