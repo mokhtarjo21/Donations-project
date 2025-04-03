4cb8cb70791be429ebd8f91de478d5357031f923
mokhtarjo21 <mokhtar.jo21@gmail.com>
2025-04-03 22:14:53 +0200

    make my branch

--- django_project/settings.py
+++ django_project/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'main_postgres',
     'USER': 'postgres',
-    'PASSWORD': 'amr', # اكتبوا الباسورد بتاع بوستجرس 
+    'PASSWORD': '123', # اكتبوا الباسورد بتاع بوستجرس 
     'HOST': 'localhost',
     'PORT': 5432,
     }
